f6c430ae790a4ce2d92c0ae18006416db770a8ed
BRENO VARGAS GEMELGO <12936310946@edu.udesc.br>
2026-03-10 16:59:28 -0300

    novo: código de aula

---
+++ analiseDeErros/erro.py
@@ -0,0 +1,59 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt 
+
+# e^x
+x = 1
+
+# número de algarismos significativos
+n = 6
+
+# Euler
+u = math.exp(x)
+
+# definição de valores
+soma = 0
+old = 0
+estimativa = []
+contador = []
+EPT = []
+EPEST = [100]
+
+# série de Maclaurin
+def Maclaurin(x, n):
+    return x ** n / math.factorial(n)
+
+# loop principal
+for i in range(n):
+    
+    # somatório
+    soma = soma + Maclaurin(x, i)
+    
+    # erros percentuais
+    Ept = abs((u - soma) / u) * 100
+    if i > 0:
+        Epest = abs((soma - old) / soma) * 100
+        EPEST.append(Epest)
+        
+    # valor antigo
+    old = soma
+        
+    # appends
+    estimativa.append(soma)
+    contador.append(i)
+    EPT.append(Ept)
+    
+plt.figure()
+plt.plot(contador, estimativa, 'or', label='$\mathrm{e}^x$')
+plt.legend()
+plt.xlabel("Número de termos")
+plt.ylabel("Estimativa")
+plt.grid()
+
+plt.figure()
+plt.plot(contador, EPT, 'og', label='$E_{pt}$')
+plt.plot(contador, EPEST, 'ob', label='$E_{pest}$')
+plt.legend()
+plt.xlabel("Número de termos")
+plt.ylabel("Erros")
+plt.grid()
