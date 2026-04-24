f5c7b1f8e972802ce476bbb48bc3028af60ea790
kreeyto <royal.ssamy@gmail.com>
2026-04-24 12:02:01 -0300

    corrige código da interpolação quadrática

--- aulas/otimizacao/interpolacaoQuadratica.py
+++ aulas/otimizacao/interpolacaoQuadratica.py
@@ -17,21 +17,29 @@ plt.grid()
 
 Eppara = 0.5 * 10 ** (2 - 6)
 Epest = 100
-x4_old = 0
-x4_new = 0
+x4_old = x2
 
 while Epest >= Eppara:
-
-    if f(x4_old) > f(x2):
-        x1 = x2
-        x2 = x4_old
-    else:
-        x3 = x4_old
 
     x4_new = x2 - 0.5 * (
         (x2 - x1) ** 2 * (f(x2) - f(x3)) - (x2 - x3) ** 2 * (f(x2) - f(x1))
     ) / ((x2 - x1) * (f(x2) - f(x3)) - (x2 - x3) * (f(x2) - f(x1)))
+
     Epest = abs((x4_new - x4_old) / x4_new) * 100
+
+    if x4_new > x2:
+        if f(x4_new) < f(x2):
+            x1 = x2
+            x2 = x4_new
+        else:
+            x3 = x4_new
+    else:
+        if f(x4_new) < f(x2):
+            x3 = x2
+            x2 = x4_new
+        else:
+            x1 = x4_new
+
     x4_old = x4_new
 
 print(x4_old)
