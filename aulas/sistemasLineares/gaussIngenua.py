cbd97245c4e1eb478b309de9036d7cb62184810f
BRENO VARGAS GEMELGO <12936310946@edu.udesc.br>
2026-05-12 15:48:37 -0300

    atualiza sistemas de interesse, adiciona .gitignore na lista de gitignores

--- aulas/sistemasLineares/gaussIngenua.py
+++ aulas/sistemasLineares/gaussIngenua.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-A = np.array([[2, 4, -6], [4, 2, 2], [2, 8, -4]])
-b = np.array([[10], [16], [24]])
+# sistema guardado 1
+# A = np.array([[3, -0.1, -0.2], [0.1, 7, -0.3], [0.3, -0.2, 10]])
+# b = np.array([[7.85], [-19.3], [71.4]])
+
+# sistema guardado 2
+A = np.array([[0.0003, 3], [1, 1]])
+b = np.array([[2.0001], [1]])
+
 Aum = np.hstack((A, b))
 n = len(b)
 
@@ -9,15 +15,13 @@ n = len(b)
 for i in range(n - 1):
     for j in range(i + 1, n):
         fator = Aum[j, i] / Aum[i, i]
-        Aum[j, range(i, n + 1)] = (
-            Aum[j, range(i, n + 1)] - fator * Aum[i, range(i, n + 1)]
-        )
+        Aum[j, i : n + 1] = Aum[j, i : n + 1] - fator * Aum[i, i : n + 1]
 
 # substituição regressiva
+soma = 0
 x = np.zeros(n)
 x[n - 1] = Aum[n - 1, n] / Aum[n - 1, n - 1]
 for i in range(n - 2, -1, -1):
-    soma = 0
     for j in range(i + 1, n):
         soma += Aum[i, j] * x[j]
     x[i] = (Aum[i, n] - soma) / Aum[i, i]
