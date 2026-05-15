e4e891425d6ee86bc61a261b9482874d5fcd427b
BRENO VARGAS GEMELGO <12936310946@edu.udesc.br>
2026-05-15 15:01:50 -0300

    corrige gauss-seidel (15/05/2026)

--- aulas/sistemasLineares/gaussSeidel.py
+++ aulas/sistemasLineares/gaussSeidel.py
@@ -7,19 +7,21 @@ import numpy as np
 A = np.array([[2, 4, -6], [4, 2, 2], [2, 8, -4]])
 b = np.array([10, 16, 24])
 
-ordem = np.argmax(np.abs(A), axis=0)
-A = A[ordem, :]
-b = b[ordem]
+# ordem = np.argmax(np.abs(A), axis=0)
+# A = A[ordem, :]
+# b = b[ordem]
 
 n = len(b)
 x_old = np.zeros(n)
 x_new = np.zeros(n)
 
 Eppara = 0.5 * 10 ** (2 - 6)
+lambida = 0.5
+maxit = 1000
 Epest = 100
 k = 0
 
-while Epest >= Eppara:
+while Epest > Eppara:
     for i in range(n):
         soma_A = 0
         soma_B = 0
@@ -31,9 +33,11 @@ while Epest >= Eppara:
                 soma_B += A[i, j] * x_old[j]
 
         x_new[i] = (b[i] - soma_A - soma_B) / A[i, i]
+        x_new[i] = lambida * x_new[i] + (1 - lambida) * x_old[i]
 
     Epest = np.max(np.abs((x_new - x_old) / x_new)) * 100
     x_old = x_new.copy()
     k += 1
 
 print(x_new)
+print(k)
